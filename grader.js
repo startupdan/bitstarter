c5781b7d77aee760e2f2afd0868d26717a769a5a
Daniel Stanojevic <danielstanojevic@gmail.com>
2013-08-08 05:39:15 +0000

    Edited product desc in index.html

--- grader.js
+++ grader.js
@@ -26,6 +26,7 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "url.html";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -55,20 +56,65 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+var checkUrl = function(url, checksfile) {
+    rest.get(url).on('complete', function(data) {
+	//console.log("in checkUrl");
+        $ = cheerio.load(data);
+        var checks = loadChecks(checksfile).sort();
+        var out = {};
+        for(var ii in checks) {
+            var present = $(checks[ii]).length > 0;
+            out[checks[ii]] = present;
+        }
+        var outJson = JSON.stringify(out, null, 4);
+        console.log(outJson);
+	//console.log("end checkUrl");
+    });
+}
+
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
     return fn.bind({});
 };
 
+var rest = require('restler');
+
+var callback = function(url) {
+   //console.log("in callback");
+   var checkJson = checkHtmlFile(url, program.checks);
+   //console.log(checkJson);
+   //console.log(url);
+   //console.log("callback complete");
+};
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <url_file>', 'url')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    if(program.url) {
+	//console.log("in if prog.url in main: program.url is: ");
+	//console.log(program.url);
+	checkUrl(program.url, program.checks);
+	//rest.get(program.url).on('complete',callback);
+	//console.log("done with prog.url in main");
+    }
+    else if (program.file) {
+	var checkJson = checkHtmlFile(program.file, program.checks);
+	var outJson = JSON.stringify(checkJson, null, 4);
+	console.log(outJson);
+    }
+    else {
+	console.log("Must have a file or url as parameter");
+    }
+    //console.log("before output");
+    //var outJson = JSON.stringify(checkJson, null, 4);
+    //console.log("just after outJson");
+    //console.log(outJson);
+    //console.log("after output of outJson");
 } else {
+    console.log("Must have a file or url as a parameter");
     exports.checkHtmlFile = checkHtmlFile;
 }
